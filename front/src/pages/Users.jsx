9fa879d6cd6c5c0b462d9a4847e447c3a85ada19
SrJacoby <luizjacoby294@gmail.com>
2025-04-23 15:55:27 -0300

    criei páginas e rotas

--- front/src/pages/Users.jsx
+++ front/src/pages/Users.jsx
@@ -3,12 +3,20 @@ import { Link } from "react-router-dom";
 import TrashCan from '../assets/trashcan.svg'
 
 const Users = () => {
+
+    const users = [
+        {
+            name: "Luiz",
+            email: "luiz@email.com"
+        }
+    ]
+
     return (
         <div className='card'>
         <div>
-          <p>Nome: <span></span></p>
-          <p>Idade: <span></span></p>
-          <p>E-mail: <span></span></p>
+          <p>Nome: <span>{users[0].name}</span></p>
+          <p>E-mail: <span>{users[0].email}</span></p>
+
         </div>
         <button><img src={TrashCan}/></button>
       </div>
